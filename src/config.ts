a8cfc09f71c035a950bd76d21b5bc6a93feb7a13
Holly <holly@dejawu.me>
2021-10-31 20:26:35 -0400

    Config and build modes

---
+++ src/config.ts
@@ -0,0 +1,53 @@
+export type Mode = "dev" | "build" | "debug";
+const configTemplate = {
+  entryPoint: "./main.ky",
+  mode: "dev" as Mode,
+};
+type ConfigTemplate = typeof configTemplate;
+const configTemplateKeys = Object.keys(configTemplate);
+
+// read CLI args
+const clArgs = process.argv.slice(2).reduce((args, arg, i) => {
+  if (i === 0 && !arg.includes(":") && !arg.includes("=")) {
+    // allow first argument to be treated as entrypoint file name
+    args["entryPoint"] = arg;
+    return args;
+  }
+
+  let argName, argVal;
+  if (arg.includes(":")) {
+    [argName, argVal] = arg.split(":");
+  } else if (arg.includes("=")) {
+    [argName, argVal] = arg.split("=");
+  } else {
+    throw new Error(
+      `Improperly formatted command-line argument: ${arg}. Please use 'argName:argValue' or 'argName=argValue'.`
+    );
+  }
+
+  args[argName] = argVal;
+  return args;
+}, {});
+
+// validate CLI config args
+Object.keys(clArgs).forEach((argName) => {
+  if (!configTemplateKeys.includes(argName)) {
+    throw new Error(`Unrecognized command-line argument: ${argName}`);
+  }
+});
+
+const configArgs = clArgs as ConfigTemplate;
+const config = {
+  ...configTemplate,
+  ...(configArgs as ConfigTemplate),
+};
+const configKeys = Object.keys(config);
+configTemplateKeys.forEach((key) => {
+  if (!configKeys.includes(key)) {
+    throw new Error(
+      `Option not provided by config file or command-line arguments: ${key}`
+    );
+  }
+});
+
+export default config as ConfigTemplate;
--- src/kytherascript.ts
+++ src/kytherascript.ts
@@ -1,15 +1,32 @@
 import { readFileSync } from "fs";
+
+import { match } from "ts-pattern";
+
+import prettier from "prettier";
+import { minify } from "terser";
+
 import compile from "./compiler";
 
-if (process.argv.length < 3) {
-  throw new Error("Expected entrypoint argument.");
-}
+import config, { Mode } from "./config";
+const { mode, entryPoint } = config;
 
-const entryPoint = process.argv[2];
+(async () => {
+  try {
+    const raw = compile(readFileSync(entryPoint).toString());
 
-try {
-  const res = compile(readFileSync(entryPoint).toString());
-  console.log(res);
-} catch (err) {
-  console.log(err);
-}
+    // this is all terser's fault
+    const out = await match<Mode, Promise<string>>(mode)
+      .with("dev", async () => raw)
+      .with(
+        "build",
+        async () => (await minify(raw, { sourceMap: false })).code || ""
+      )
+      .with("debug", () => prettier.format(raw, { parser: "babel" }))
+      .otherwise((mode) => {
+        throw new Error(`Invalid build mode: ${mode}`);
+      });
+    console.log(out);
+  } catch (err) {
+    console.log(err);
+  }
+})();
